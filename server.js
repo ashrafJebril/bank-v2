25ad5499e71b6f2b801b0f1323b36597266dae3e
a-abukhadir <abdallaha@estartasolutions.com>
2023-02-18 15:41:06 +0300

    update uri

--- server.js
+++ server.js
@@ -18,28 +18,28 @@ api.getProducts();
 getProductsWithDetails = async () => {
   // return await myController.getProductsWithDetials();
 };
-app.get("/banks", async (req, res) => {
+app.get("/banks-v2/banks", async (req, res) => {
   const result = await controller.create();
   res.status(result.status);
   delete result.status;
   res.send(result);
 });
-app.get("/products", async (req, res, next) => {
+app.get("/banks-v2/products", async (req, res, next) => {
   res.send(await getProductsWithDetails());
 });
-app.get("/bank-products/:id", async (req, res) => {
+app.get("/banks-v2/bank-products/:id", async (req, res) => {
   const result = await controller.getProductsByBankId(req.params.id);
   res.status(result.status);
   delete result.status;
   res.send(result);
 });
-app.get("/bank/:id", async (req, res) => {
+app.get("/banks-v2/bank/:id", async (req, res) => {
   const result = await controller.getBankById(req.params.id);
   res.status(result.status);
   delete result.status;
   res.send(result);
 });
-app.get("/product/:id", async (req, res) => {
+app.get("/banks-v2/product/:id", async (req, res) => {
   const result = await controller.getProductDetailsById(req.params.id);
   res.status(result.status);
   delete result.status;

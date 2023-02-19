8099a0c15aca1d8f0cf2e072c58bc0886bd0bbd2
Ashraf Jebril <ajebril@aiims.com.au>
2023-02-20 10:47:25 +1100

    fix

--- models/product_details.js
+++ models/product_details.js
@@ -8,10 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // product_details.belongsTo(models.products, {
-      //   as: "product",
-      //   foreignKey: "productId",
-      // });
+      product_details.belongsTo(models.products, {
+        as: "product",
+        foreignKey: "productId",
+      });
     }
   }
   product_details.init(
